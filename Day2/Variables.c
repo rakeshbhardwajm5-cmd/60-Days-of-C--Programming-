517f3327a43a471249436a8589de96857cb08e55
Rakesh bharadwaj <rakeshbhardwajm5@gmail.com>
2026-07-20 23:55:00 +0530

    Change character name and age in Variables.c

--- Day2/Variables.c
+++ Day2/Variables.c
@@ -1,8 +1,14 @@
-char characterName[] = "Tom";
-int characterAge = 67;
+char characterName[] = "Raki";
+int characterAge = 34;
 printf("There once was a man named %s\n", characterName);
 printf("he was %d years old.\n", characterAge);
 characterAge = 30;
 printf("He really liked the name %s\n", characterName);
 printf("but did not like being %d.\n", characterAge);
 return 0;
+
+#output 
+There once was a man named Raki
+he was 34 years old.
+He really liked the name Raki
+but did not like being 30.
