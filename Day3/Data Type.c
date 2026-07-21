8eda673a75616d10ba518bd76f40e44d30c65667
Rakesh bharadwaj <rakeshbhardwajm5@gmail.com>
2026-07-21 16:53:46 +0530

    Add variables and print statements for user info

--- Day3/Data Type.c
+++ Day3/Data Type.c
@@ -1,1 +1,8 @@
-
+int Num = 30;
+char Name = 'R';
+double Cgpa = 7.9;
+char phase [] = "My name is Raki";
+printf("My name is Raki  My Cgpa is %d", 7.9);
+printf("My name is %s is Raki My cgpa is %d", 'Raki', 8.0);
+char somename [] = 'i';
+printf("My name is %c is Raki My cgpa is %d", somename , 8.0);
